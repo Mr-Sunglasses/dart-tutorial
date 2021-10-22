0c7daabeb3c8df79fd3a7c2e21ad40c47ff9453a
Kanishk Pachauri <krishna03012003@icloud.com>
2021-10-22 20:18:41 +0530

    prefix

---
+++ bin/prefix_and_postfix.dart
@@ -0,0 +1,26 @@
+void main(){
+
+  // prefix Increment
+  var i = 3;
+  print(i);
+  print(++i);
+  print(i);
+
+  print("------------------------------------");
+  // postfix Increment
+  var j = 4;
+  print(j);
+  print(j++);
+  print(j);
+
+
+  print("--------------------------------------");
+  // Finally shown both prefix and postfix increment and decrement
+  var k = 20;
+  print(++k);
+  print(--k);
+  print(k++);
+  print(k--);
+  print(k);
+
+}
