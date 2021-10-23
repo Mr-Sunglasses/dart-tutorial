5fc4a6d542d7d75bd0773b800eeb83e59e5b0741
Kanishk Pachauri <krishna03012003@icloud.com>
2021-10-23 21:58:33 +0530

    added lists

---
+++ bin/lists.dart
@@ -0,0 +1,8 @@
+void main() {
+  // List
+  // Fixed length list
+  List<int> mylist = [1, 2, 3, 4, 5];
+  List<String> mylist2 = ["Hello", "World"];
+  print(mylist.runtimeType);
+  print(mylist2.runtimeType);
+}
