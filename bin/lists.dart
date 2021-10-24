7d5fdb0e3322b265c636ac7382c4c742ada3ec58
Kanishk Pachauri <krishna03012003@icloud.com>
2021-10-24 23:54:57 +0530

    learning list:

--- bin/lists.dart
+++ bin/lists.dart
@@ -3,6 +3,7 @@ void main() {
   // Fixed length list
   List<int> mylist = [1, 2, 3, 4, 5];
   List<String> mylist2 = ["Hello", "World"];
+  List<bool> myboollist = [true, false, true];
   print(mylist.runtimeType);
   print(mylist2.runtimeType);
 }
